e2447690b17b56fc233ad6ac39c409f54a3b57b7
skalermo <skalermo@gmail.com>
2019-11-26 09:41:43 +0100

    Start progress chart

--- Main.py
+++ Main.py
@@ -1,6 +1,9 @@
 import argparse
 from time import time
 from sys import stdout
+import matplotlib.pyplot as plt
+
+
 from Generator import genGraph
 from AiUtils import nextGeneration, initPopulation, calcDistance
 from Graph import WeightedGraph as Graph
@@ -12,21 +15,26 @@ def evolutionaryAlgorithm(graph, generations, eliteSize, mutationRate):
     population = initPopulation(popSize, graph.vertices())
     lastDistance = -1
     lastGeneration = 0
+    progress = []
+    if lastDistance != -1:
+        progress.append(calcDistance(graph, population[0]))
     try:
         for i in range(generations):
             population = nextGeneration(graph, population, eliteSize, mutationRate)
             distance = calcDistance(graph, population[0])
+            if lastDistance != -1:
+                progress.append(calcDistance(graph, population[0]))
 
             if lastDistance != distance:
+                lastDistance = distance
                 print()
 
             stdout.write(u"\u001b[1000DGeneration:{} Distance:{} ".format(i, distance))
             stdout.flush()
 
-            lastDistance = distance
             lastGeneration = i
     finally:
-        return population[0], calcDistance(graph, population[0]), lastGeneration
+        return population[0], progress, lastGeneration
 
 
 if __name__ == '__main__':
@@ -39,6 +47,7 @@ if __name__ == '__main__':
     parser.add_argument('-m', type=float, default=None, metavar='', help='Chance of mutation on given gene')
     parser.add_argument('-i', type=str, metavar='', help='Input file')
     parser.add_argument('-o', type=str, default=None, metavar='', help='Output file')
+    parser.add_argument('-c', action='store_true', default=False, help='Output a chart')
 
     args = vars(parser.parse_args())
     graph = None
@@ -67,10 +76,9 @@ if __name__ == '__main__':
         mutationRate = 10.0 / vertexCount
 
     startTime = time()
-
-    bestPath, bestDistance, lastGeneration = evolutionaryAlgorithm(graph, generations, eliteSize, mutationRate)
-
+    bestPath, progress, lastGeneration = evolutionaryAlgorithm(graph, generations, eliteSize, mutationRate)
     end = int((time() - startTime) * 1000)
+    bestDistance = progress[-1]
     print('\nTime: {}m {}s {}ms'.format((end // 60000), (end // 1000) % 60, end % 1000))
     if args['o'] is None:
         print(str(bestPath) + ' ' + str(bestDistance))
@@ -78,3 +86,15 @@ if __name__ == '__main__':
         with open(args['o'], 'w') as output:
             output.write('Last generation: {}, Distance: {}\n'.format(lastGeneration, bestDistance))
             output.write('Path: {}, \' {}\' \n'.format(str(bestPath)[1:-1], str(bestPath[0])))
+
+    if args['c']:
+        a = [progress[-1]] * (lastGeneration - len(progress))
+        a += progress
+        plt.plot(a)
+        plt.ylabel('Distance')
+        plt.xlabel('Generation')
+        # fig = plt.gcf()
+        # fig.set_size_inches(18, 10, forward=True)
+        # fig.set_dpi(100)
+        # fig.savefig('test1.png', dpi=100)
+        plt.show()
